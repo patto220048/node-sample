3783a60545b3540d19a8e49b96174533855114a2
patto220048 <220048xn@yse-c.net>
2023-10-10 11:02:28 +0900

    add mail service

--- src/sever.js
+++ src/sever.js
@@ -1,8 +1,13 @@
-import express from 'express';
+
+const express = require('express');
 const app = express();
-import * as dotenv from 'dotenv';
+const path = require('path');
+const  dotenv = require('dotenv');
 dotenv.config()
-const port = 4000 || process.env.PORT
+const port = 4001 || process.env.PORT
+
+app.use(express.static(path.join(__dirname, '/public')))
+
 app.get('/', (req, res) => {
     // クライアンのからのリクエスト
     console.log(req.body)
@@ -11,6 +16,11 @@ app.get('/', (req, res) => {
 
     // サーバーからのレスポンス
     res.send('Hello!!!!')
+})  
+app.get('/profile', (req, res) => {
+    res.send('Welcome your profile')
+    console.log(req.url)
+
 })
 
 app.listen(port, () => console.log(`Listening on port ${port} : ${process.env.HOST}:${port}`));
